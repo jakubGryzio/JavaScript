1564f69c2d2455c0eeee675d0382fa77c225b1ac
JakubGryzioHex <jakub.gryzio@hexagon.com>
2021-09-29 15:50:10 +0200

    Updated scripts
    Develop new project and tutorial script

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+    const outputString = [];
+
+    for (let i = 1; i <= arr.length; i++) {
+        outputString.push(`... ${arr[i - 1]} \u235BC in ${i} days`);
+    }
+    return outputString.join(' ')
+}
+
+const forecast = printForecast([12, 5, -5, 0, 4]);
+console.log(forecast);
--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 13;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+    const checkGreatness = function (how) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = `Too ${how}!`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = 'You lost the game!';
+        }
+    }
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number! 🛑';
+    } else if (guess > secretNumber) {
+        checkGreatness('high');
+    } else if (guess < secretNumber) {
+        checkGreatness('low');
+    } else {
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    }
+});
+
