bd1a54b3a81d1b52a886c8df326427ef430e0897
JakubGryzioHex <jakub.gryzio@hexagon.com>
2021-09-30 20:49:55 +0200

    Update script.js
    Updating RandomNumber project's script.

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,16 +1,8 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 13;
-*/
-
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
     const checkGreatness = function (how) {
@@ -36,6 +28,21 @@ document.querySelector('.check').addEventListener('click', () => {
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.message').textContent = 'Correct Number! 🎉';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
     }
 });
 
+document.querySelector('.again').addEventListener('click', () => {
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    score = 20;
+
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
