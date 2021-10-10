c8f3aebd9f964e2b9a11b94366d4974705497a1d
JakubGryzio <jakubgryzio@gmail.com>
2021-10-10 12:06:39 +0200

    Updating new project's script
    Adding functionality to new project: rolling dice, display dice as an image, game logic

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//Selecting elements
+const score0Elem = document.querySelector('#score--0');
+const score1Elem = document.getElementById('score--1');
+const diceElem = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting conditions
+score0Elem.textContent = 0;
+score1Elem.textContent = 0;
+diceElem.classList.add('hidden');
+
+let currentScore1 = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceElem.src = `dice-${dice}.png`;
+  diceElem.classList.remove('hidden');
+
+  // 3. Check for rolled 1:
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore1 += dice;
+  } else {
+    //switch to next player
+  }
+});
