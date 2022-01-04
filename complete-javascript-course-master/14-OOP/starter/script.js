b8f8f9f2924384b5b5dd938c27e8920cdd4e85c2
JakubGryzio <jakubgryzio@gmail.com>
2022-01-04 23:55:41 +0100

    OOP base learning

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
+/* const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
@@ -8,6 +8,9 @@ const Person = function (firstName, birthYear) {
   //   this.calcAge = function () {
   //     return new Date().getFullYear() - this.birthYear;
   //   };
+  Person.prototype.calcAge = function () {
+    console.log(new Date().getFullYear() - this.birthYear);
+  };
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -23,3 +26,127 @@ const jakub = new Person('Jakub', 1999);
 console.log(jakub);
 
 console.log(jakub instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(new Date().getFullYear() - this.birthYear);
+};
+console.log(Person.prototype);
+
+jonas.calcAge();
+matilda.calcAge();
+jakub.calcAge();
+console.log(jonas.__proto__.calcAge);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jakub.species, jonas.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jakub.__proto__);
+console.log(jakub.__proto__.__proto__);
+console.log(jakub.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 4, 5, 6, 6];
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1); */
+
+// function contsfunc() {
+//   var varr = [];
+//   for (var i = 0; i < 3; i++) {
+//     varr[i] = function () {
+//       return i;
+//     };
+//   }
+//   i = 100;
+//   return varr;
+// }
+
+// var varr = contsfunc();
+// console.log(varr[1]());
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} speed up to ${this.speed} km/h`);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} speed down to ${this.speed} km/h`);
+};
+
+const audi = new Car('Audi', 60);
+console.log(audi.accelerate());
+console.log(audi.break());
+console.log(audi.break());
+console.log(audi.accelerate());
+*/
+// clas expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(new Date().getFullYear() - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jakub = new PersonCl('Jakub', 1999);
+jakub.calcAge();
+
+console.log(jakub.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jakub.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
